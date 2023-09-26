aa3a85f3c0c61321122ac7b2f7723a1e831e1a22
Yisak <yisakbruck23@gmail.com>
2023-09-26 13:30:57 -0400

    complete lab

--- server/app.py
+++ server/app.py
@@ -1,42 +1,60 @@
-#!/usr/bin/env python3
+
 
 from flask import request, session
 from flask_restful import Resource
-
 from config import app, db, api
 from models import User
 
 class ClearSession(Resource):
-
     def delete(self):
-    
         session['page_views'] = None
         session['user_id'] = None
-
         return {}, 204
 
 class Signup(Resource):
-    
     def post(self):
-        json = request.get_json()
-        user = User(
-            username=json['username'],
-            password_hash=json['password']
-        )
-        db.session.add(user)
-        db.session.commit()
-        return user.to_dict(), 201
+        try:
+            json = request.get_json()
+            user = User(username=json['username'])
+            user.password_hash = json['password']
+            db.session.add(user)
+            db.session.commit()
+            session['user_id'] = user.id
+            return {"username": user.username, "id": user.id}, 201
+        except Exception as e:
+            return {"error": str(e)}, 500
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        user_id = session.get('user_id')
+        if not user_id:
+            return {}, 204
+        
+        user = db.session.get(User, user_id)
+        if not user:
+            return {}, 204
+
+        return user.to_dict(), 200
 
 class Login(Resource):
-    pass
+    def post(self):
+        json = request.get_json()
+        user = User.query.filter_by(username=json['username']).first()
+
+        if user and user.authenticate(json['password']):
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+        else:
+            return {"message": "Invalid credentials"}, 401
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        session['user_id'] = None  
+        return {}, 204
 
-api.add_resource(ClearSession, '/clear', endpoint='clear')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
 api.add_resource(Signup, '/signup', endpoint='signup')
 
 if __name__ == '__main__':
